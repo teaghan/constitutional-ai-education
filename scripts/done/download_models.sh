e00570e98c40dbee668926161dea6ed7094b7c4b
Teaghan <obriaintb@gmail.com>
2025-04-16 15:42:23 -0400

    cleanup

--- scripts/done/download_models.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module load python/3.12.4
-module load cuda/12.2
-module load arrow/19.0.1
-source /home/obriaint/project/obriaint/const_ai/bin/activate
-
-python /home/obriaint/scratch/constitutional-ai-education/download_model.py /home/obriaint/project/obriaint/gemma-3-4b-it-unsloth-bnb-4bit
